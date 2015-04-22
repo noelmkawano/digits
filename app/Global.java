c82cb0e8d54acdfe2ea1062a9d9a8d0ddff3eba9
Noel Kawano <noelmk@hawaii.edu>
2015-04-21 23:37:28 -1000

    finished

--- app/Global.java
+++ app/Global.java
@@ -27,18 +27,21 @@ public class Global extends GlobalSettings {
     ContactDB.addDietType(new DietType("Gluten"));
 
     List<String> testDiet = new ArrayList<>();
-    testDiet.add(0, "Beef");
-    ContactDB.addContacts(new ContactFormData("Patrick", "Karjala", "808-222-3333", "Mobile", testDiet));
+    testDiet.add(0, "Fish");
+    testDiet.add(1, "Chicken");
+    testDiet.add(2, "Beef");
+    ContactDB.addContacts(new ContactFormData("Noel", "Kawano", "808-123-4567", "Mobile", testDiet));
+
     List<String> testDiet2 = new ArrayList<>();
-    testDiet2.add(0, "Chicken");
+    testDiet2.add(0, "Dairy");
     testDiet2.add(1, "Gluten");
-    ContactDB.addContacts(new ContactFormData("Brooke", "Jones", "808-111-2222", "Mobile", testDiet2));
+    ContactDB.addContacts(new ContactFormData("Katherine", "Kawano", "808-123-4568", "Work", testDiet2));
+
     List<String> testDiet3 = new ArrayList<>();
-    ContactDB.addContacts(new ContactFormData("Bradford", "Jones", "254-879-0744", "Home", testDiet3));
+    testDiet3.add(0, "Beef");
+    ContactDB.addContacts(new ContactFormData("Noah", "Kawano", "808-123-4569", "Home", testDiet3));
+
     List<String> testDiet4 = new ArrayList<>();
-    testDiet4.add(0, "Dairy");
-    testDiet4.add(1, "Beef");
-    testDiet4.add(2, "Fish");
-    ContactDB.addContacts(new ContactFormData("Bud", "Jones", "808-123-4567", "Work", testDiet4));
+    ContactDB.addContacts(new ContactFormData("Darryl", "Kawano", "808-123-4560", "Work", testDiet4));
   }
 }
